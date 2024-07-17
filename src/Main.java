55eb47b7b4b4459add85d671f5298d16b7e6f711
Fouziya-1903 <118104880+Fouziya-1903@users.noreply.github.com>
2024-07-17 19:50:17 +0530

    Hii..

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello there!! Who is this?");
+        Scanner sc = new scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hi" + name);
+    }
+}
